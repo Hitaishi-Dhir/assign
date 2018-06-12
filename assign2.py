081acf064f4598a934ee5e62111e98935a9f1634
Hitaishi Dhir <dhirhitaishi@gmail.com>
2018-06-12 19:47:18 +0530

    assignment2

---
+++ assign2.py
@@ -0,0 +1,33 @@
+#q1
+print("hey!! acadview is just awesome")
+
+#q2
+print("acad"+"view")
+#a="acad"
+#b="view"
+#print(a+b)
+
+
+#q3
+x=input("enter value of x")
+y=int(input("enter value of y"))
+z=int(input("enter value of z"))
+print(x)
+print(y)
+print(z)
+
+#q4
+print("let's get started")
+#print('let\'s get started')
+
+#q5
+s ="acadview"
+course ="python"
+fees =5000
+
+print("institude name is %s, course name is %s, fees is %d"%(s,course,fees))
+
+#q6
+name ="tony stark"
+salary=1000000
+print('%s %d'%(name,salary))
